2aa1ee8949d5d9828023e02e5c4a3a75f84f8769
Christian Blum <chris@pfersdorff-blum.de>
2024-10-31 13:16:17 +0100

    wir benutzen mechanische Tasten und müssen sie entprellen

--- src/tasten.h
+++ src/tasten.h
@@ -9,4 +9,24 @@ extern volatile bool semaphore_taste_effekte_ein_aus;
 void tasten_setup();
 void tasten_loop();
 
+
+
+class Taste {
+  private:
+    int count;
+    int pin;
+    bool zustand;
+    bool aenderung;
+
+  public:
+    Taste(int pin);
+
+    void begin();
+    void loop();
+    bool aktiv();
+    bool zustandswechsel();
+};
+
+
+
 #endif
